7b91c61b2adc492682956e9fcea72c69cb6352a3
Milana <milanin7147@gmail.com>
2022-07-25 21:54:01 +0400

    some corrections

--- specs/all_tests.spec.ts
+++ specs/all_tests.spec.ts
@@ -11,12 +11,12 @@ describe("kinopoisk.ru testing", () => {
     it("Check input field", async () => {
         await (await getWebElement(MainPage.searchInput)).setValue("Игра престолов");
         await (await getWebElement(MainPage.searchButton)).click();
-        const checkResult = await (await getWebElement(ResultPage.result));
+        const checkResult = await getWebElement(ResultPage.result);
         await expect(await checkResult.isDisplayed());
     });
     it("Check the subscription link", async () => {
         await (await getWebElement(MainPage.searchLink)).click();
-        const checkButton = await (await getWebElement(ResultPage.subscribe));
+        const checkButton = await getWebElement(ResultPage.subscribe);
         await expect(await checkButton.isEnabled());
     });
     it("Check getText", async () => {
@@ -27,12 +27,12 @@ describe("kinopoisk.ru testing", () => {
     });
     it("Check buying tickets", async () => {
         await (await getWebElement(MainPage.searchBuyingTickets)).click();
-        const checkClickable = await (await getWebElement((ResultPage.resultOfButton)));
+        const checkClickable = await getWebElement(ResultPage.resultOfButton);
         await expect(await checkClickable.isClickable());
     });
     it("Check random search", async () => {
         await (await getWebElement(MainPage.findTheSearchButton)).click();
-        const checkRandomSearch = await (await getWebElement(ResultPage.randomSearch));
+        const checkRandomSearch = await getWebElement(ResultPage.randomSearch);
         await expect(await checkRandomSearch.isExisting());
     });
 });
